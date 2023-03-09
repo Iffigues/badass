2d3b1828a692272326312fb5945a3be10b25f7dd
Malbanes <morgane.albanese@gmail.com>
2023-03-09 16:18:59 +0100

    add gns3 priject

--- badass/P1/install.sh
+++ badass/P1/install.sh
@@ -1,2 +1,2 @@
-docker build -t v1/busy -f /home/user42/bureau/v1/Dockerfile.busy /home/user42/bureau/v1
-docker build -t v1/frr -f /home/user42/bureau/v1/Dockerfile.frr /home/user42/bureau/v1
+docker build -t v1/busy -f /home/user42/bureau/P1/Dockerfile.busy /home/user42/bureau/P1
+docker build -t v1/frr -f /home/user42/bureau/P1/Dockerfile.frr /home/user42/bureau/P1
