858be23639df6015dc7fe3fb51ad19a205269e2b
Malbanes <morgane.albanese@gmail.com>
2023-07-17 15:30:41 +0200

    add script setup for vm

--- badass/P1/setup_vm.sh
+++ badass/P1/setup_vm.sh
@@ -1,3 +1,6 @@
+    #su
+    #adduser user42 sudo
+    #exit
     sudo apt-get install -y git
     sudo apt-get install -y vim
     sudo apt-get install -y curl
@@ -7,5 +10,8 @@
 
     sudo apt install python3-pip python3-pyqt5 python3-pyqt5.qtsvg python3-pyqt5.qtwebsockets qemu qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst wireshark xtightvncviewer apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
     sudo pip3 install gns3-server gns3-gui
-    sudo usermod -aG kvm,libvirt,docker,wireshark "$USER"
+    curl https://raw.githubusercontent.com/GNS3/gns3-server/master/scripts/remote-install.sh > gns3-remote-install.sh
+    sudo bash gns3-remote-install.sh
+
+    sudo usermod -aG kvm,libvirt,ubridge,docker,wireshark "$USER"
     echo "gns3 installed."
