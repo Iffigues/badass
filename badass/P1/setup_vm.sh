424b5dc2c78f383b9af77324a3f6da62d7c2aa54
Malbanes <morgane.albanese@gmail.com>
2023-07-17 14:10:50 +0200

    add script setup for vm

--- badass/P1/setup_vm.sh
+++ badass/P1/setup_vm.sh
@@ -3,9 +3,9 @@
     sudo apt-get install -y curl
     sudo apt-get install -y whireshark
     sudo apt-get install -y Python3
+    sudo apt-get install -y docker-compose
+
     sudo apt install python3-pip python3-pyqt5 python3-pyqt5.qtsvg python3-pyqt5.qtwebsockets qemu qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst wireshark xtightvncviewer apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
     sudo pip3 install gns3-server gns3-gui
-    sudo systemctl disable gns3.service
-    sudo usermod -aG kvm,libvirt,docker,ubridge,wireshark "$USER"
+    sudo usermod -aG kvm,libvirt,docker,wireshark "$USER"
     echo "gns3 installed."
-    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'gns3.desktop', 'wireshark.desktop', 'org.gnome.Terminal.desktop']"
